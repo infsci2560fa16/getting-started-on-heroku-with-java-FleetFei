ea008b6ba6359ad94e7bd9dc35b7c947b15fec0f
FleetFei <qifeish@gmail.com>
2016-12-13 22:58:27 -0500

    qifei

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import java.sql.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
 
-<<<<<<< HEAD
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -8,10 +11,6 @@ import spark.template.freemarker.FreeMarkerEngine;
 import spark.ModelAndView;
 import static spark.Spark.get;
 
-import static javax.measure.unit.SI.KILOGRAM;
-import javax.measure.quantity.Mass;
-import org.jscience.physics.model.RelativisticModel;
-import org.jscience.physics.amount.Amount;
 
 public class Main {
     
@@ -20,95 +19,62 @@ public class Main {
         port(Integer.valueOf(System.getenv("PORT")));
         staticFileLocation("/public");
         
-        get("/hello", (req, res) -> {
-            RelativisticModel.select();
-            Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-            return "E=mc^2: 12 GeV = " + m.toString();
-        });
+         get("/hello", (req, res) -> {
+             RelativisticModel.select();
+             Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+             return "E=mc^2: 12 GeV = " + m.toString();
+         });
         
         get("/", (request, response) -> {
-=======
 
-public class Main {
-
-  public static void main(String[] args) {
-
-    port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
-
-    get("/hello", (req, res) -> {
-          RelativisticModel.select();
-          Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-          return "E=mc^2: 12 GeV = " + m.toString();
-        });
-
-    get("/", (request, response) -> {
->>>>>>> ddac846759afb2e9d4ad5a8724da5d5210685727
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
             
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
-<<<<<<< HEAD
+
         
-        get("/db", (req, res) -> {
-            Connection connection = null;
-            Map<String, Object> attributes = new HashMap<>();
-            try {
-                connection = getConnection();
+         get("/db", (req, res) -> {
+             Connection connection = null;
+             Map<String, Object> attributes = new HashMap<>();
+             try {
+                 connection = getConnection();
                 
-                Statement stmt = connection.createStatement();
-                stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-                stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-                ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+                 Statement stmt = connection.createStatement();
+                 stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+                 stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+                 ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
                 
-                ArrayList<String> output = new ArrayList<String>();
-                while (rs.next()) {
-                    output.add( "Read from DB: " + rs.getTimestamp("tick"));
-                }
+                 ArrayList<String> output = new ArrayList<String>();
+                 while (rs.next()) {
+                     output.add( "Read from DB: " + rs.getTimestamp("tick"));
+                 }
                 
-                attributes.put("results", output);
-                return new ModelAndView(attributes, "db.ftl");
-            } catch (Exception e) {
-                attributes.put("message", "There was an error: " + e);
-                return new ModelAndView(attributes, "error.ftl");
-            } finally {
-                if (connection != null) try{connection.close();} catch(SQLException e){}
-            }
-        }, new FreeMarkerEngine());
+                 attributes.put("results", output);
+                 return new ModelAndView(attributes, "db.ftl");
+             } catch (Exception e) {
+                 attributes.put("message", "There was an error: " + e);
+                 return new ModelAndView(attributes, "error.ftl");
+             } finally {
+                 if (connection != null) try{connection.close();} catch(SQLException e){}
+             }
+         }, new FreeMarkerEngine());
         
     }
     
-    private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-        int port = dbUri.getPort();
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
+    // private static Connection getConnection() throws URISyntaxException, SQLException {
+    //     URI dbUri = new URI(System.getenv("DATABASE_URL"));
+    //     int port = dbUri.getPort();
+    //     String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
         
-        if (dbUri.getUserInfo() != null) {
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-            return DriverManager.getConnection(dbUrl, username, password);
-        } else {
-            return DriverManager.getConnection(dbUrl);
-=======
+    //     if (dbUri.getUserInfo() != null) {
+    //         String username = dbUri.getUserInfo().split(":")[0];
+    //         String password = dbUri.getUserInfo().split(":")[1];
+    //         return DriverManager.getConnection(dbUrl, username, password);
+    //     } else {
+    //         return DriverManager.getConnection(dbUrl);
 
-
-    get("/db", (req, res) -> {
-      Connection connection = null;
-      Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = DatabaseUrl.extract().getConnection();
-
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
->>>>>>> ddac846759afb2e9d4ad5a8724da5d5210685727
-        }
-    }
+    //     }
+    // }
     
 }
